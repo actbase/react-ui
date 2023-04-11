18c0b9315eb761df1aec09e8c78b5b91bacbc95f
Seung Ju <kingsky32@gmail.com>
2023-04-11 11:56:59 +0900

    initial commit

--- components/theme/index.ts
+++ components/theme/index.ts
@@ -1,12 +1,1 @@
-import Provider from './provider';
-import context from './context';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ThemeProps {}
-
-const Theme = {
-  context,
-  Provider,
-};
-
-export default Theme;
+export { default } from './theme';
---
+++ components/theme/theme.types.ts
@@ -0,0 +1,12 @@
+export interface ThemeProps {
+  size: 10;
+  components: {
+    button: {};
+    divider: {};
+    form: {};
+    input: {};
+    space: {};
+    table: {};
+    typography: {};
+  };
+}
