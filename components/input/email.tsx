54b65a8111919d7abc061f4778f6a3c35fb0e7de
김동규 <solitare@actbase.io>
2023-04-12 16:17:20 +0900

    fix: email forward generics

--- components/input/email.tsx
+++ components/input/email.tsx
@@ -4,30 +4,29 @@ import Theme from '../theme';
 import { ClassNames } from '@emotion/react';
 import getClassName from '../_util/getClassName';
 
-const InputEmail = React.forwardRef(function InputEmail({
-  className,
-  placeholder,
-  ...props
-}: InputEmailProps) {
-  const theme = Theme.useContext();
-  return (
-    <ClassNames>
-      {({ css, cx }) => (
-        <input
-          type="email"
-          className={cx(
-            css`
-              ${theme?.components?.input?.style}
-            `,
-            getClassName(theme?.namespace, 'input__email'),
-            className,
-          )}
-          placeholder={placeholder}
-          {...props}
-        />
-      )}
-    </ClassNames>
-  );
-});
+const InputEmail = React.forwardRef<HTMLInputElement, InputEmailProps>(
+  function InputEmail({ className, placeholder, ...props }, ref) {
+    const theme = Theme.useContext();
+    return (
+      <ClassNames>
+        {({ css, cx }) => (
+          <input
+            ref={ref}
+            type="email"
+            className={cx(
+              css`
+                ${theme?.components?.input?.style}
+              `,
+              getClassName(theme?.namespace, 'input__email'),
+              className,
+            )}
+            placeholder={placeholder}
+            {...props}
+          />
+        )}
+      </ClassNames>
+    );
+  },
+);
 
 export default InputEmail;
