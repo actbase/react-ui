803acd4bc25f5c03121dd588410c78aebac9422a
김동규 <solitare@actbase.io>
2023-04-12 12:30:00 +0900

    fix: add export types.ts

--- components/input/file.types.ts
+++ components/input/file.types.ts
@@ -1,8 +1,6 @@
 import { InputProps } from './input.types';
 
-interface FileProps extends InputProps {
-  // 받아들이는 파일 형식
-  accept: string[];
+export interface FileProps extends InputProps {
   // 업로드 할 수 있는 파일의 최대크기
   maxSize?: number;
   // 업로드 할 수 있는 파일의 최대 갯수
--- components/input/textarea.types.ts
+++ components/input/textarea.types.ts
@@ -1,6 +1,6 @@
 import { InputProps } from './input.types';
 
-interface TextAreaProps extends InputProps {
+export interface TextAreaProps extends InputProps {
   // 사이즈 조절 가능여부
   resize?: boolean;
   // 텍스트 에어리어 세로 폭
