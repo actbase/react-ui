39d97bdb4f34a761c8ddcebf1858780f281f6c63
Seung Ju <kingsky32@gmail.com>
2023-04-14 15:04:43 +0900

    feat: ui

--- components/form/item.stories.tsx
+++ components/form/item.stories.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import InitialFormItem from './item';
+import InternalFormItem from './item';
 import Input from '../input';
+import { Meta, StoryObj } from '@storybook/react';
 
 export default {
   title: 'Data Entry/Form',
-  component: InitialFormItem,
+  component: InternalFormItem,
   args: {
     label: 'label',
-    children: <Input placeholder="placeholder" />,
     error: 'error',
   },
+} satisfies Meta<typeof InternalFormItem>;
+
+type Story = StoryObj<typeof InternalFormItem>;
+export const Item: Story = {
+  render: (args) => (
+    <InternalFormItem {...args}>
+      <Input placeholder="placeholder" />
+    </InternalFormItem>
+  ),
 };
-export const Item = {};
