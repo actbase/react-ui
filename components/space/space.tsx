3b80fdaf8074e3f377f90ad3581cda3a2dc1b7ae
Seung Ju <kingsky32@gmail.com>
2023-04-19 15:43:01 +0900

    Revert "refact: element"
    This reverts commit 178e718ab45d2afe06ac390da1ce9a8e39b7c8c7.

--- components/space/space.tsx
+++ components/space/space.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { SpaceProps } from './space.types';
+import { ClassNames } from '@emotion/react';
 import Theme from '../theme';
-import useCreateElement from '../element/hooks/useCreateElement';
+import getClassName from '../_util/getClassName';
+import getNamespace from '../_util/getNamespace';
+import mergeStyles from '../_util/mergeStyles';
 
 const Space = React.forwardRef(function Space<
   T extends keyof JSX.IntrinsicElements,
@@ -17,56 +20,89 @@ const Space = React.forwardRef(function Space<
     direction,
     wrap,
     reverse,
+    type,
+    style,
     htmlType,
+    css: _css,
     ...props
   }: SpaceProps<T>,
   ref: React.ForwardedRef<JSX.IntrinsicElements[T]>,
 ) {
   const theme = Theme.useContext();
   const Element = el ?? 'div';
-  return useCreateElement('space', ({ cx, css }) => (
-    // @ts-ignore
-    <Element
-      ref={ref}
-      className={cx(
-        css`
-          display: ${inline ? 'inline-flex' : 'flex'};
-          justify-content: ${{
-            start: 'flex-start',
-            center: 'center',
-            end: 'flex-end',
-            'space-between': 'space-between',
-            'space-around': 'space-around',
-            'space-evenly': 'space-evenly',
-          }[justify ?? 'start']};
-          flex-direction: ${{
-            horizontal: reverse ? 'row-reverse' : 'row',
-            vertical: reverse ? 'column-reverse' : 'column',
-          }[direction ?? 'horizontal']};
-          flex-wrap: ${wrap ? 'wrap' : 'no-wrap'};
-          align-items: ${{
-            start: 'flex-start',
-            center: 'center',
-            end: 'flex-end',
-            stretch: 'stretch',
-          }[align ?? 'stretch']};
-          ${size &&
-          `gap: ${
-            typeof size === 'number'
-              ? `${size}px`
-              : `${
-                  theme?.components?.space?.size?.[size] ??
-                  theme?.components?.space?.defaultSize
-                }px`
-          }`}
-        `,
-        className,
-      )}
-      {...props}
-    >
-      {children}
-    </Element>
-  ));
+  return (
+    <ClassNames>
+      {({ css, cx }) => {
+        return (
+          // @ts-ignore
+          <Element
+            // @ts-ignore
+            ref={ref}
+            style={mergeStyles(
+              theme?.components?.space?.style,
+              type && theme?.components?.space?.type?.[type]?.style,
+              style,
+            )}
+            // @ts-ignore
+            className={cx(
+              getNamespace(theme?.namespace),
+              css`
+                display: ${inline ? 'inline-flex' : 'flex'};
+                justify-content: ${{
+                  start: 'flex-start',
+                  center: 'center',
+                  end: 'flex-end',
+                  'space-between': 'space-between',
+                  'space-around': 'space-around',
+                  'space-evenly': 'space-evenly',
+                }[justify ?? 'start']};
+                flex-direction: ${{
+                  horizontal: reverse ? 'row-reverse' : 'row',
+                  vertical: reverse ? 'column-reverse' : 'column',
+                }[direction ?? 'horizontal']};
+                flex-wrap: ${wrap ? 'wrap' : 'no-wrap'};
+                align-items: ${{
+                  start: 'flex-start',
+                  center: 'center',
+                  end: 'flex-end',
+                  stretch: 'stretch',
+                }[align ?? 'stretch']};
+                gap: ${typeof size === 'number'
+                  ? `${size}px`
+                  : `${
+                      // @ts-ignore
+                      theme?.components?.space?.size?.[size] ??
+                      theme?.components?.space?.defaultSize
+                    }px`};
+                ${typeof theme?.components?.space?.css === 'function'
+                  ? theme.components.space.css({ color: theme?.color })
+                  : theme?.components?.space?.css};
+                ${type &&
+                (typeof theme?.components?.space?.type?.[type]?.css ===
+                'function'
+                  ? // @ts-ignore
+                    theme.components.space.type[type].css({
+                      color: theme?.color,
+                    })
+                  : theme?.components?.space?.type?.[type]?.css)};
+                ${typeof _css === 'function'
+                  ? _css({ color: theme?.color })
+                  : _css};
+              `,
+              getClassName(theme?.namespace, 'space'),
+              theme?.components?.space?.className,
+              type && getClassName(theme?.namespace, `space__type__${type}`),
+              type && theme?.components?.space?.type?.[type]?.className,
+              className,
+            )}
+            {...props}
+          >
+            {children}
+          </Element>
+        );
+      }}
+    </ClassNames>
+  );
 });
 
 export default Space;
