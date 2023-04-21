7241661d1b416c4537781bf2e1b40dfe10015d4b
김동규 <solitare@actbase.io>
2023-04-21 16:25:20 +0900

    docs: input radio

--- components/input/radio.stories.tsx
+++ components/input/radio.stories.tsx
@@ -5,7 +5,33 @@ import InternalRadio from './radio';
 export default {
   title: 'Data Entry/Input',
   args: {
+    className: '',
+    type: '',
     children: 'radio',
+    checked: true,
+  },
+  argTypes: {
+    className: {
+      name: 'className',
+      description: 'css class name',
+    },
+    type: {
+      name: 'type',
+      description: 'type',
+    },
+    children: {
+      name: 'text',
+      description: 'string',
+      defaultValue: '',
+    },
+    htmlType: {
+      name: 'htmlType',
+      description: 'html input type',
+    },
+    checked: {
+      name: 'checked',
+      description: 'boolean',
+    },
   },
   component: InternalRadio,
 } satisfies Meta<typeof InternalRadio>;
