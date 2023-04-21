fddce97d8e5004d3fe73df52d98af24d0ee87652
김동규 <solitare@actbase.io>
2023-04-21 17:15:02 +0900

    docs: typography

--- components/typography/typography.stories.tsx
+++ components/typography/typography.stories.tsx
@@ -7,7 +7,32 @@ export default {
   title: 'General/Typography',
   args: {
     children: 'Typography',
+    weight: 400,
     type: 'h1',
+    size: 16,
+  },
+  argTypes: {
+    children: {
+      name: 'text',
+      description: 'string',
+    },
+    weight: {
+      name: 'font-weight',
+      description: 'font weight',
+      control: {
+        type: 'select',
+      },
+      options: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+    size: {
+      name: 'font-size',
+      description: 'font size',
+    },
+  },
+  parameters: {
+    controls: {
+      exclude: ['css', 'el', 'type'],
+    },
   },
   component: InitialTypography,
 } satisfies Meta<typeof InitialTypography>;
