1b34e3598d952809bdf401c9be42f138457e0416
김동규 <solitare@actbase.io>
2023-04-24 12:28:45 +0900

    docs: button loading, disable props add

--- components/button/button.stories.tsx
+++ components/button/button.stories.tsx
@@ -10,19 +10,36 @@ export default {
     children: 'children',
     type: 'primary',
     htmlType: 'button',
+    radius: 0,
   },
   argTypes: {
     className: {
-      name: 'className',
+      name: 'css className',
       description: 'string',
     },
     htmlType: {
       name: 'htmlType',
-      description: 'html button type',
+      description: 'type of a button is exposed on the component',
       control: {
         type: 'select',
       },
       options: ['submit', 'reset', 'button'],
+    },
+    radius: {
+      name: 'radius',
+      description: 'border-radius value',
+    },
+    loading: {
+      name: 'loading',
+      description: 'describe button loading state `boolean`',
+      type: 'boolean',
+      default: false,
+    },
+    disabled: {
+      name: 'disabled',
+      description: 'describe button disabled state `boolean`',
+      type: 'boolean',
+      default: false,
     },
   },
   component: InitialButton,
